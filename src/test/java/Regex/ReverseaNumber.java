c29b760a16e3d5c02b6ca76b2d37f2095898bc90
bgadkane <bgadkane@gmail.com>
2023-01-31 20:30:34 +0530

    Logic updated

--- src/test/java/Regex/ReverseaNumber.java
+++ src/test/java/Regex/ReverseaNumber.java
@@ -4,16 +4,14 @@ public class ReverseaNumber {
 
 	public static void main(String[] args) {
 
-		int a = 123;
-    int rev=0;
-		System.out.println("The number before reverse is : " + a);
-    while(num!=0)
-    {
-    rev=rev*10+num%10;
-    num=num/10;
-    }
-		System.out.println("The number after reverse is :"+rev);
+		int num = 789;
+		int rev = 0;
+		System.out.println("The number before reverse is : " + num);
+		while (num != 0) {
+			rev = rev * 10 + num % 10;
+			num = num / 10;
+		}
+		System.out.println("The number after reverse is : " + rev);
 
 	}
-
 }
