7ac7ca0310e3680667b8bb1c85a0367acc991878
Baburao Adkane <122772376+learnby23@users.noreply.github.com>
2023-01-31 20:09:31 +0530

    Update SwapaNumber.java

--- src/test/java/Regex/SwapaNumber.java
+++ src/test/java/Regex/SwapaNumber.java
@@ -8,15 +8,15 @@ public class SwapaNumber {
 		int b = 20;
 		System.out.println("The number before swapping as a: " + a + " and b: " + b);
 
-		// int temp = a; a = b; b = temp; 	//Logic 1
+		// int temp = a; a = b; b = temp;   	//Logic 1
 
-		// a = a + b; b = a - b; a = a - b; //Logic 2
+		// a = a + b; b = a - b; a = a - b; 	//Logic 2
 
-		// a = a * b; b = a / b; a = a / b; //Logic 3
+		// a = a * b; b = a / b; a = a / b;	//Logic 3
 
-		// a = a ^ b; b = a ^ b; a = a ^ b; //Logic 4
+		// a = a ^ b; b = a ^ b; a = a ^ b; 	//Logic 4
 
-		b = a + b - (a = b); 				// Logic 5
+		b = a + b - (a = b); 			//Logic 5
 
 		System.out.println("The number after swapping as a: " + a + " and b: " + b);
 
