a532e26e4501adb7d13cedcb1fb29224bd3f0e64
Baburao Adkane <122772376+learnby23@users.noreply.github.com>
2023-02-01 17:39:13 +0530

    Update ExtractNumber.java

--- src/test/java/Regex/ExtractNumber.java
+++ src/test/java/Regex/ExtractNumber.java
@@ -3,14 +3,14 @@ package Regex;
 public class ExtractNumber {
 
 	public static void main(String agrs[]) {
-		String str = "A7B8C9%";
+		String string = "A7B8C9%";
 		String output = "";
-		System.out.println("The given string is: " + str);
+		System.out.println("The given string is: " + string);
 
-		for (int i = 0; i <= str.length() - 1; i++) {
+		for (int i = 0; i <= string.length() - 1; i++) {
 
-			if (Character.isDigit(str.charAt(i))) {
-				output = output + str.charAt(i);
+			if (Character.isDigit(string.charAt(i))) {
+				output = output + string.charAt(i);
 			}
 		}
 		System.out.println("The extracted number is: " + output);
